4303098866f30218f58630635dbfe7aebe72b008
John Kloor <john@kloor.org>
2022-11-16 11:10:16 -0500

    Make date navigation into a trait for reuse.

---
+++ src/Lits/Action/DateTrait.php
@@ -0,0 +1,159 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lits\Action;
+
+use Lits\Config\DepoConfig;
+use Lits\Exception\InvalidDataException;
+use Safe\DateTimeImmutable;
+use Safe\Exceptions\DatetimeException;
+use Slim\Exception\HttpBadRequestException;
+
+use function Latitude\QueryBuilder\func;
+
+trait DateTrait
+{
+    private ?DateTimeImmutable $date = null;
+    private ?DateTimeImmutable $date_first = null;
+    private ?DateTimeImmutable $date_previous = null;
+    private ?DateTimeImmutable $date_next = null;
+    private ?DateTimeImmutable $date_last = null;
+
+    /** @throws HttpBadRequestException */
+    public function date(
+        string $table = 'item',
+        string $field = 'updated'
+    ): DateTimeImmutable {
+        if (\is_null($this->date)) {
+            $this->date_first = $this->findDateFirst($table, $field);
+            $this->date_last = $this->findDateLast($table, $field);
+            $this->date = $this->findDate();
+
+            if (\is_null($this->date)) {
+                throw new HttpBadRequestException(
+                    $this->request,
+                    'Invalid date requested'
+                );
+            }
+
+            try {
+                $this->date_previous = $this->date->modify('previous month');
+                $this->date_next = $this->date->modify('next month');
+            } catch (DatetimeException $exception) {
+                // @ignoreException
+            }
+        }
+
+        return $this->date;
+    }
+
+    /**
+     * @return array<string, DateTimeImmutable|null>
+     * @throws HttpBadRequestException
+     */
+    public function dateContext(
+        string $table = 'item',
+        string $field = 'updated'
+    ): array {
+        return [
+            'date' => $this->date($table, $field),
+            'date_first' => $this->date_first,
+            'date_previous' => $this->date_previous,
+            'date_next' => $this->date_next,
+            'date_last' => $this->date_last,
+        ];
+    }
+
+    private function findDateFirst(
+        string $table,
+        string $field
+    ): ?DateTimeImmutable {
+        if ($table === 'item' && $field === 'updated') {
+            \assert($this->settings['depo'] instanceof DepoConfig);
+
+            return $this->settings['depo']->start;
+        }
+
+        $statement = $this->database->execute(
+            $this->database->query
+                ->select(func('MIN', $field))
+                ->from($table)
+        );
+
+        try {
+            return self::firstDayOfMonth(
+                (string) $statement->fetchColumn()
+            );
+        } catch (InvalidDataException $exception) {
+            // @ignoreException
+        }
+
+        return null;
+    }
+
+    private function findDateLast(
+        string $table,
+        string $field
+    ): ?DateTimeImmutable {
+        $statement = $this->database->execute(
+            $this->database->query
+                ->select(func('MAX', $field))
+                ->from($table)
+        );
+
+        try {
+            return self::firstDayOfMonth(
+                (string) $statement->fetchColumn()
+            );
+        } catch (InvalidDataException $exception) {
+            // @ignoreException
+        }
+
+        return null;
+    }
+
+    private function findDate(): ?DateTimeImmutable
+    {
+        $date = null;
+
+        try {
+            /** @var ?string $param */
+            $param = $this->request->getQueryParam('date');
+
+            $date = self::firstDayOfMonth($param);
+        } catch (InvalidDataException $exception) {
+            // @ignoreException
+        }
+
+        if (\is_null($date) || $date > $this->date_last) {
+            return $this->date_last;
+        }
+
+        if ($date < $this->date_first) {
+            return $this->date_first;
+        }
+
+        return $date;
+    }
+
+    /** @throws InvalidDataException */
+    private static function firstDayOfMonth(?string $date): ?DateTimeImmutable
+    {
+        if (\is_null($date)) {
+            return null;
+        }
+
+        try {
+            $date = new DateTimeImmutable($date);
+
+            return $date->modify('first day of this month');
+        } catch (\Throwable $exception) {
+            throw new InvalidDataException(
+                'Could not determine first day of month',
+                0,
+                $exception
+            );
+        }
+    }
+}
--- src/Lits/Action/MonthAction.php
+++ src/Lits/Action/MonthAction.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace Lits\Action;
 
-use Lits\Config\DepoConfig;
 use Lits\Data\CatalogData;
 use Safe\DateTimeImmutable;
 use Safe\Exceptions\DatetimeException;
 use Slim\Exception\HttpBadRequestException;
 use Slim\Exception\HttpInternalServerErrorException;
-use Throwable;
 
 use function Latitude\QueryBuilder\alias;
 use function Latitude\QueryBuilder\field;
@@ -18,6 +16,8 @@ use function Latitude\QueryBuilder\func;
 
 final class MonthAction extends AuthAction
 {
+    use DateTrait;
+
     /**
      * @throws HttpInternalServerErrorException
      * @throws HttpBadRequestException
@@ -44,60 +44,23 @@ final class MonthAction extends AuthAction
      */
     private function context(): array
     {
-        \assert($this->settings['depo'] instanceof DepoConfig);
-
-        $date_first = $this->settings['depo']->start;
-
-        $statement = $this->database->execute(
-            $this->database->query
-                ->select(func('MAX', 'updated'))
-                ->from('item')
-        );
-
-        $date_last = (string) $statement->fetchColumn();
+        $date = $this->date();
 
         try {
-            $date_last = new DateTimeImmutable($date_last);
-            $date_last = $date_last->modify('first day of this month');
-
-            /** @var ?string $date */
-            $date = $this->request->getQueryParam('date');
-
-            if (!\is_string($date)) {
-                $date = $date_last;
-            } else {
-                $date = new DateTimeImmutable($date);
-                $date = $date->modify('first day of this month');
-            }
-
-            $date_previous = $date->modify('previous month');
-            $date_next = $date->modify('next month');
             $total = $this->total($date);
-        } catch (Throwable $exception) {
+        } catch (\Throwable $exception) {
             throw new HttpBadRequestException(
                 $this->request,
-                'Could not determine date range',
+                'Could not retrieve totals',
                 $exception
             );
         }
 
-        if ($date < $date_first || $date > $date_last) {
-            throw new HttpBadRequestException(
-                $this->request,
-                'Invalid date requested'
-            );
-        }
-
-        return [
+        return $this->dateContext() + [
             'catalogs' => CatalogData::all(
                 $this->settings,
                 $this->database
             ),
-            'date' => $date,
-            'date_first' => $date_first,
-            'date_previous' => $date_previous,
-            'date_next' => $date_next,
-            'date_last' => $date_last,
             'total' => $total,
         ];
     }
